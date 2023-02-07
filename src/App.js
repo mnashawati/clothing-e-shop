7721e8f18ade270fb80236a5eb943077122e8c5b
Mo Nashawati <mo.nashawati@capgemini.com>
2023-02-07 22:56:44 +0000

    add shop component & route to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+// import Shop from './routes/shop/shop.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
 
@@ -8,6 +15,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
