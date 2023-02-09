50f1984b2a4413f2a09d4819484e0465cdce98ce
Mo Nashawati <mo.nashawati@capgemini.com>
2023-02-09 23:08:45 +0000

    add firebase utils

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo7PoBbM6tiMRry3OqgRLqdiF8nomye9s",
+  authDomain: "clothing-e-shop-db.firebaseapp.com",
+  projectId: "clothing-e-shop-db",
+  storageBucket: "clothing-e-shop-db.appspot.com",
+  messagingSenderId: "176220815555",
+  appId: "1:176220815555:web:2935bb7f9b94f1e3196276"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
