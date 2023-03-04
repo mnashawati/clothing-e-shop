3217f0b9be70e28889374d4b38606edb2e1fc6ac
Mo Nashawati <mo.nashawati@capgemini.com>
2023-03-04 14:58:32 +0000

    add sign-in with google using firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +16,17 @@ const firebaseConfig = {
   projectId: "clothing-e-shop-db",
   storageBucket: "clothing-e-shop-db.appspot.com",
   messagingSenderId: "176220815555",
-  appId: "1:176220815555:web:2935bb7f9b94f1e3196276"
+  appId: "1:176220815555:web:a47de193794f1eeb196276",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
